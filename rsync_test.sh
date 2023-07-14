0719c842267ab4e272d674d749a6a775bd37351b
JerryMoo <wuchujie123@gmail.com>
2023-07-14 18:19:45 +0800

    update: update rsync

--- rsync_test.sh
+++ rsync_test.sh
@@ -1,2 +1,5 @@
-rsync -avP -e "ssh" http-proxy-forward deploy@168.63.153.150:/home/deploy/vhost/http-proxy-forward/
-rsync -avP -e "ssh" socks-proxy-forward deploy@168.63.153.150:/home/deploy/vhost/socks-proxy-forward/
+# rsync -avP -e "ssh" http-proxy-forward deploy@168.63.153.150:/home/deploy/vhost/http-proxy-forward/
+# rsync -avP -e "ssh" socks-proxy-forward deploy@168.63.153.150:/home/deploy/vhost/socks-proxy-forward/
+
+rsync -avP -e "ssh" http-proxy-forward proxy302-jump-server:/home/azureuser/vhost/http-proxy-forward/
+rsync -avP -e "ssh" socks-proxy-forward proxy302-jump-server:/home/azureuser/vhost/socks-proxy-forward/
